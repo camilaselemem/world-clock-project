1a92b9260bf5342ebce957abd43491452a2595d9
camilaselemem <camilaselemem@hotmail.com>
2024-10-23 09:58:05 +0100

    add current location

--- javascript/index.js
+++ javascript/index.js
@@ -31,6 +31,9 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -42,7 +45,7 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format(
             "hh:mm:ss"
-          )}<small>${cityTime.format("A")}</small></div>
+          )}<small>${cityTime.format(" A")}</small></div>
         </div>`;
 }
 
